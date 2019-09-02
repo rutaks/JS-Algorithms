48b3203d8d4b931cb1a483574832d02381827162
rutaks <rootsum.dev@gmail.com>
2019-09-02 06:11:38 +0200

    Minor Changes

--- merged_string_checker.js
+++ merged_string_checker.js
@@ -1,3 +1,11 @@
+//An algorithm to check if a given string, s,
+//can be formed from two other strings, part1 and part2
+//Only if the characters in part1 and part2 are in the same order as in s
+// eg: 'codewars' is a merge from 'cdw' and 'oears':
+//     s:  c o d e w a r s   = codewars
+// part1:  c   d   w         = cdw
+// part2:    o   e   a r s   = oears
+
 function isMerge(str, p1, p2) {
 	if (!str.length) return !p1.length && !p2.length;
 	if (p1.length && str.charAt(0) == p1.charAt(0)) {
